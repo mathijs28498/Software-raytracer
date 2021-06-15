b7a65a0067a7efa486b46d035741dfe4c56dc144
mathijs28498 <33691132+mathijs28498@users.noreply.github.com>
2021-06-15 19:50:19 +0200

    Added basic shadows

--- raytracer/src/Main.cpp
+++ raytracer/src/Main.cpp
@@ -3,8 +3,21 @@
 #include <cmath>
 #include <vector>
 #include <limits>
+#include <chrono>
+#include <windows.h>
+#include <wingdi.h>
+
 
 # define PI           3.14159265358979323846  /* pi */
+
+constexpr int WIDTH = 500;
+constexpr int HEIGHT = 500;
+
+float invWidth = 1 / float(WIDTH), invHeight = 1 / float(HEIGHT);
+float fov = 90, aspectratio = WIDTH / float(HEIGHT);
+float angle = fov * PI / 180;
+float imagePlaneWidth = 100;
+float imagePlaneHeight = aspectratio * imagePlaneWidth;
 
 struct Vec3 {
 	float x, y, z;
@@ -26,6 +39,8 @@ struct Vec3 {
 	Vec3 operator* (float a) { return Vec3(x * a, y * a, z * a); }
 	Vec3 operator* (Vec3 v) { return Vec3(x * v.x, y * v.y, z * v.z); }
 	Vec3 operator/ (float a) { return Vec3(x / a, y / a, z / a); }
+	Vec3 operator- () { return Vec3{ -x, -y, -z }; }
+	bool operator== (Vec3 v) { return abs(x - v.x) < 0.001f && abs(y - v.y) < 0.001f && abs(z - v.z) < 0.001f; }
 };
 
 float dot(Vec3 a, Vec3 b) {
@@ -40,6 +55,7 @@ struct Color {
 	Color operator* (float a) { return Color(rgb * a); };
 	Color operator* (Color c) { return Color(rgb * c.rgb); };
 	Color operator+ (Color c) { return Color((rgb + c.rgb) / 2); };
+	bool operator== (Color c) { return rgb == c.rgb; };
 };
 
 struct Ray {
@@ -57,7 +73,7 @@ public:
 	Sphere(Vec3 center, float radius, Color color) : center(center), radius(radius), color(color) {};
 
 	Vec3 getNormal(Vec3 intersec) {
-		return (intersec - center) / radius;
+		return ((intersec - center) / radius).normalize();
 	}
 
 	bool intersect(Ray ray, float& t) {
@@ -71,80 +87,162 @@ public:
 		float t0 = -b - disc;
 		float t1 = -b + disc;
 		t = t0 < t1 ? t0 : t1;
-		return true;
+		return t > 0;
 	}
 };
 
+void rayTrace0(std::vector<Sphere>* spheres, Sphere* light, HDC* hdc) {
+	std::vector<Color> pixels(HEIGHT * WIDTH);
+
+	float imagePlaneRatio = WIDTH / imagePlaneWidth;
+	Vec3 cameraPosition{ 0, 0, 10 };
+	bool firstLoop = true;
+
+	while (true) {
+		for (int y = 0; y < HEIGHT; y++) {
+			for (int x = 0; x < WIDTH; x++) {
+				float xc = x - WIDTH / 2;
+				float yc = y - HEIGHT / 2;
+
+				Vec3 imagePlanePoint{ xc / imagePlaneRatio, yc / imagePlaneRatio, -10 };
+
+
+				Vec3 direction = (imagePlanePoint - cameraPosition).normalize();
+				Ray primaryRay{ cameraPosition, direction };
+
+
+
+				float minDist = 2000000;
+				Color endColor;
+				for (size_t i = 0; i < spheres->size(); i++) {
+					Sphere sphere = (*spheres)[i];
+					float dist;
+					if (sphere.intersect(primaryRay, dist) && dist < minDist) {
+						minDist = dist;
+
+						Vec3 intersec = primaryRay.origin + primaryRay.dir * dist;
+						Vec3 lightDir = (light->center - intersec).normalize();
+						Vec3 normal = sphere.getNormal(intersec);
+
+						float strength = dot(lightDir.normalize(), normal.normalize());
+
+						endColor = sphere.color * 0.7f + light->color * strength * 1.1f;
+
+						if (endColor.rgb.x < 0) endColor.rgb.x = 0;
+						if (endColor.rgb.y < 0) endColor.rgb.y = 0;
+						if (endColor.rgb.z < 0) endColor.rgb.z = 0;
+						if (endColor.rgb.x > 255) endColor.rgb.x = 255;
+						if (endColor.rgb.y > 255) endColor.rgb.y = 255;
+						if (endColor.rgb.z > 255) endColor.rgb.z = 255;
+					}
+				}
+				if (!(endColor == pixels[y * WIDTH + x]) || firstLoop) {
+					SetPixel(*hdc, x, y, RGB(endColor.rgb.x, endColor.rgb.y, endColor.rgb.z));
+					pixels[y * WIDTH + x] = endColor;
+				}
+			}
+		}
+		//cameraPosition = cameraPosition + Vec3{ 0, 1, 0 };
+		//spheres[0].center = spheres[0].center + Vec3{ -2, 0, 0 };
+		firstLoop = false;
+		//std::fill(pixels.begin(), pixels.end(), Color());
+	}
+}
+
+void rayTrace1(std::vector<Sphere>* spheres, Sphere* light, HDC* hdc) {
+	std::vector<Color> pixels(HEIGHT * WIDTH);
+
+	float imagePlaneRatio = WIDTH / imagePlaneWidth;
+	Vec3 cameraPosition{ 0, 0, 10 };
+	bool firstLoop = true;
+	while (true) {
+		for (int y = 0; y < HEIGHT; y++) {
+			for (int x = 0; x < WIDTH; x++) {
+				float xc = x - WIDTH / 2;
+				float yc = y - HEIGHT / 2;
+
+				Vec3 imagePlanePoint{ xc / imagePlaneRatio, yc / imagePlaneRatio, -10 };
+
+				Vec3 direction = (imagePlanePoint - cameraPosition).normalize();
+				Ray primaryRay{ cameraPosition, direction };
+				float minDist = 2000000;
+				Color endColor;
+
+				Sphere* hitSphere = nullptr;
+				Vec3 intersec;
+				Vec3 normal;
+				size_t sphereIndex;
+
+				for (size_t i = 0; i < spheres->size(); i++) {
+					Sphere* sphere = &(*spheres)[i];
+					float dist;
+					if (sphere->intersect(primaryRay, dist) && dist < minDist) {
+						minDist = dist;
+
+						intersec = primaryRay.origin + primaryRay.dir * dist;
+						Vec3 lightDir = (light->center - intersec).normalize();
+						normal = sphere->getNormal(intersec);
+
+						hitSphere = sphere;
+						sphereIndex = i;
+					}
+				}
+
+				if (hitSphere != nullptr) {
+					Ray shadowRay{ intersec ,(light->center - intersec).normalize() };
+					bool isShadow = false;
+
+					for (size_t i = 0; i < spheres->size(); i++) {
+						if (i == sphereIndex) continue;
+						Sphere sphere = (*spheres)[i];
+						float tempDist;
+						if (sphere.intersect(shadowRay, tempDist)) {
+							isShadow = true;
+							break;
+						}
+					}
+
+					if (!isShadow) {
+						float strength = dot(shadowRay.dir, normal.normalize());
+
+						endColor = hitSphere->color * 0.7f + light->color * strength * 1.1f;
+
+						if (endColor.rgb.x < 0) endColor.rgb.x = 0;
+						if (endColor.rgb.y < 0) endColor.rgb.y = 0;
+						if (endColor.rgb.z < 0) endColor.rgb.z = 0;
+						if (endColor.rgb.x > 255) endColor.rgb.x = 255;
+						if (endColor.rgb.y > 255) endColor.rgb.y = 255;
+						if (endColor.rgb.z > 255) endColor.rgb.z = 255;
+
+					}
+				}
+
+				if (!(endColor == pixels[y * WIDTH + x]) || firstLoop) {
+					SetPixel(*hdc, x, y, RGB(endColor.rgb.x, endColor.rgb.y, endColor.rgb.z));
+					pixels[y * WIDTH + x] = endColor;
+				}
+			}
+		}
+
+		(*spheres)[0].center = (*spheres)[0].center + Vec3{1, 0, 0};
+		firstLoop = false;
+	}
+
+
+}
+
 int main() {
-	constexpr int WIDTH = 500;
-	constexpr int HEIGHT = 500;
 
-	std::ofstream out("output/trace.ppm");
-	out << "P3\n" << WIDTH << "\n" << HEIGHT << "\n255\n";
-
-	std::vector<Color> pixelColors;
-	pixelColors.resize(HEIGHT * WIDTH);
+	HDC hdc = GetDC(GetConsoleWindow());
 
 	std::vector<Sphere> spheres{
-		{ {0, 0, -10}, 5, {{255, 0, 255}}  },
-		{ {10, 20, -10}, 7, {{0, 255, 0}} },
+		{ {0, 0, -5}, 10, {{255, 0, 255}}  },
+		{ {10, 20, -20}, 20, {{0, 255, 0}} },
 		{ {-10, -20, -30}, 25, {{255, 255, 0}} },
 	};
 
-	Sphere light{ {15, 0, 0}, 5, {{255, 255, 255}} };
+	Sphere light{ {10, 0, 10}, 5, {{255, 255, 255}} };
 
-	//float fov = PI / 4, aspectRatio = (WIDTH + 0.0f) / HEIGHT;
-
-	float invWidth = 1 / float(WIDTH), invHeight = 1 / float(HEIGHT);
-	float fov = 90, aspectratio = WIDTH / float(HEIGHT);
-	float angle = fov * PI / 180;
-	float imagePlaneWidth = 100;
-	float imagePlaneHeight = aspectratio * imagePlaneWidth;
-
-	float imagePlaneRatio = WIDTH / imagePlaneWidth;
-
-	for (int y = 0; y < HEIGHT; y++) {
-		for (int x = 0; x < WIDTH; x++) {
-			float xc = x - WIDTH / 2;
-			float yc = y - HEIGHT / 2; 
-
-			Vec3 imagePlanePoint{ xc / imagePlaneRatio, yc / imagePlaneRatio, -10 };
-			Vec3 cameraPosition{0, 0, 0};
-
-			Vec3 direction = (imagePlanePoint - cameraPosition).normalize();
-			Ray primaryRay{ cameraPosition, direction };
-
-			float minDist = std::numeric_limits<float>::max();
-			for (Sphere sphere : spheres) {
-				float dist;
-				if (sphere.intersect(primaryRay, dist) && dist < minDist) {
-					minDist = dist;
-
-					Vec3 intersec = primaryRay.origin + primaryRay.dir * dist;
-
-
-					Vec3 lightDir = light.center - intersec;
-					Vec3 normal = sphere.getNormal(intersec);
-					float strength = dot(lightDir.normalize(), normal.normalize());
-
-					Color endColor = sphere.color * 0.7f + light.color * strength * 1.1f;
-
-					if (endColor.rgb.x < 0) endColor.rgb.x = 0;
-					if (endColor.rgb.y < 0) endColor.rgb.y = 0;
-					if (endColor.rgb.z < 0) endColor.rgb.z = 0;
-					if (endColor.rgb.x > 255) endColor.rgb.x = 255;
-					if (endColor.rgb.y > 255) endColor.rgb.y = 255;
-					if (endColor.rgb.z > 255) endColor.rgb.z = 255;
-
-					pixelColors[y * WIDTH + x] = endColor;
-				}
-			}
-			out << static_cast<int>(pixelColors[y * WIDTH + x].rgb.x) << '\n';
-			out << static_cast<int>(pixelColors[y * WIDTH + x].rgb.y) << '\n';
-			out << static_cast<int>(pixelColors[y * WIDTH + x].rgb.z) << '\n';
-		}
-	}
-	out.close();
-
+	rayTrace1(&spheres, &light, &hdc);
 	return 0;
 }
